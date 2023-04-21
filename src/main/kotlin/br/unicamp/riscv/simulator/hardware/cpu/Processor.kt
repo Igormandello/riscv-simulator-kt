9fe1e84f4bc52eac53f99db5416b9863db40cb4d
igormandello <igormandello@gmail.com>
2023-04-21 18:00:55 -0300

    feat: adds processor execution

--- src/main/kotlin/br/unicamp/riscv/simulator/hardware/cpu/Processor.kt
+++ src/main/kotlin/br/unicamp/riscv/simulator/hardware/cpu/Processor.kt
@@ -2,7 +2,16 @@ package br.unicamp.riscv.simulator.hardware.cpu
 
 import br.unicamp.riscv.simulator.hardware.Memory
 import br.unicamp.riscv.simulator.hardware.RegisterFile
+import br.unicamp.riscv.simulator.hardware.cpu.decoding.Decoder
+import br.unicamp.riscv.simulator.model.PC
 
-class Processor(private val memory: Memory, private val registerFile: RegisterFile) {
-//    fun ByteArray.decode
+class Processor(private val memory: Memory, private val registerFile: RegisterFile, private val decoder: Decoder) {
+    fun execute() {
+        while (registerFile[PC] > 0u) {
+            val pcAddress = registerFile[PC]
+            val fetchedInstruction = memory.loadWord(pcAddress)
+            val instruction = decoder.decodeInstruction(fetchedInstruction)
+            instruction.execute(registerFile, memory)
+        }
+    }
 }

1564277efc48bc2c063be5eef5cecbbb08cce9fc
igormandello <igormandello@gmail.com>
2023-04-21 18:24:26 -0300

    feat: adds rv32m extension decoding

--- src/main/kotlin/br/unicamp/riscv/simulator/hardware/cpu/decoding/RTypeInstructionWord.kt
+++ src/main/kotlin/br/unicamp/riscv/simulator/hardware/cpu/decoding/RTypeInstructionWord.kt
@@ -10,6 +10,10 @@ class RTypeInstructionWord(word: Word) : InstructionWord(word) {
     override val imm: Word = 0u
 
     override fun decode(): Instruction {
+        if (funct7 == 0b1u) {
+            return decodeRV32MExtensionInstruction()
+        }
+
         when (funct3) {
             0b010u, 0b011u -> return SetIfLessThan(funct3 == 0b010u, rd, rs1, rs2)
         }
@@ -22,6 +26,22 @@ class RTypeInstructionWord(word: Word) : InstructionWord(word) {
             0b110u -> BinaryOpKind.OR
             0b111u -> BinaryOpKind.AND
             else -> throw IllegalArgumentException("Unknown R Type function")
+        }
+
+        return BinaryOp(kind, rd, rs1, rs2)
+    }
+
+    private fun decodeRV32MExtensionInstruction(): Instruction {
+        val kind = when (funct3) {
+            0b000u -> BinaryOpKind.MUL
+            0b001u -> BinaryOpKind.MULH
+            0b010u -> BinaryOpKind.MULHSU
+            0b011u -> BinaryOpKind.MULHU
+            0b100u -> BinaryOpKind.DIV
+            0b101u -> BinaryOpKind.DIVU
+            0b110u -> BinaryOpKind.REM
+            0b111u -> BinaryOpKind.REMU
+            else -> throw IllegalArgumentException("Unknown RV32M Extension function")
         }
 
         return BinaryOp(kind, rd, rs1, rs2)

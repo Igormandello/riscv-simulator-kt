c72cfdcf15897036d73c13f2fe83ca6a879c2513
igormandello <igormandello@gmail.com>
2023-04-21 17:25:08 -0300

    feat: adds instruction decoding

---
+++ src/main/kotlin/br/unicamp/riscv/simulator/hardware/cpu/Processor.kt
@@ -0,0 +1,8 @@
+package br.unicamp.riscv.simulator.hardware.cpu
+
+import br.unicamp.riscv.simulator.hardware.Memory
+import br.unicamp.riscv.simulator.hardware.RegisterFile
+
+class Processor(private val memory: Memory, private val registerFile: RegisterFile) {
+//    fun ByteArray.decode
+}

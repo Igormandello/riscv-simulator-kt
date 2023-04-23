490d720651f8d16a0b819007598ed7ba1c35c4aa
igormandello <igormandello@gmail.com>
2023-04-23 19:41:51 -0300

    feat: adds main tests

--- src/main/kotlin/br/unicamp/riscv/simulator/Main.kt
+++ src/main/kotlin/br/unicamp/riscv/simulator/Main.kt
@@ -1,5 +1,23 @@
 package br.unicamp.riscv.simulator
 
+import br.unicamp.riscv.simulator.hardware.Memory
+import br.unicamp.riscv.simulator.hardware.RegisterFile
+import br.unicamp.riscv.simulator.hardware.cpu.Processor
+import br.unicamp.riscv.simulator.log.Logger
+import java.io.File
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    File("./test/build/elf/")
+        .listFiles()
+        ?.sorted()
+        ?.forEach {
+            println("Processing ${it.name}...")
+            val registerFile = RegisterFile()
+            val memory = Memory()
+            val logger = Logger(registerFile, "./test/${it.nameWithoutExtension}.log")
+            val processor = Processor(memory, registerFile, logger)
+
+            it.readBytes().forEachIndexed { i, byte -> memory.storeByte(i.toUInt(), byte.toUByte()) }
+            processor.execute()
+        }
 }
